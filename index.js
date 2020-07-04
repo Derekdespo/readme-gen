252d9aad76150dcb912866e05c3e03847c5939bd
Derek Desposito <derekdespo@icloud.com>
2020-07-03 22:05:21 -0400

    just getting the repository set up, more work to come

---
+++ index.js
@@ -0,0 +1,20 @@
+// set const for modules that we will require
+const inquire = require('inquirer');
+const fs = require('fs');
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
