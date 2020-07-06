0f73a3a0ef83408568d75a99f0bcca83fe6a5054
Derek Desposito <derekdespo@icloud.com>
2020-07-05 21:41:14 -0400

    waited to commit because wasn't sure if working properly

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# readme-gen
+
+    # Meh
+
+    ## Description
+
+    its pretty meh
+
+    ## Table of Contents
+    * [Installation](##installation)
+    * [Usage](##usage)
+    * [License](##license)
+    * [Contributers](##contributers)
+    * [Tests](##tests)
+
+    ## Installation
+
+    step one: meh
+
+    ## Usage
+
+    meh mhe blah blah
+
+    ## License
+
+    MIT
+
+    ## Contributors
+
+    undefined
+
+    ## Tests
+    
+    n/a
--- index.js
+++ index.js
@@ -1,72 +1,133 @@
 // set const for modules that we will require
 const inquire = require('inquirer');
 const fs = require('fs');
+const util = require('util')
 const { title } = require('process');
 
+const writeFileAsync = util.promisify(fs.writeFile);
 // array of questions for user
-const questions = [
+// const questions = [
+function propmtUser() {
+    return inquire.prompt ([
+
 {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?"
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
 },
 {
-    type: "input",
-    name: "description",
-    message: "Describe your project!"
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project!'
+},
+// {
+//     type: 'input',
+//     name: 'tableOfContents',
+//     message: 'Input section names to create a table of contents.'
+// },
+{
+    type: 'input',
+    name: 'install',
+    message: 'Please input the steps required for installing your project.'
 },
 {
-    type: "input",
-    name: "tableOfContents",
-    message: "Input section names to create a table of contents."
-},
-{
-    type: "input",
-    name: "install",
-    message: "Please input the steps required for installing your project."
-},
-{
-    type: "input",
-    name: "usage",
-    message: "Provide instructions for the user on how to navigate your project."
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions for the user on how to navigate your project.'
 },
 {
     type: "input",
     name: "license",
-    message: "Provide any licenses associated with your project."
+    message: 'Provide any licenses associated with your project.'
 },
 {
-    type: "input",
-    name: "badge",
-    message: "Provide any badges associated with your project."
+    type: 'input',
+    name: 'badge',
+    message: 'Provide any badges associated with your project.'
 },
 {
-    type: "input",
-    name: "contribute",
-    message: "List any contributors on your project."
+    type: 'input',
+    name: 'contribute',
+    message: 'List any contributors on your project.'
 },
 {
-    type: "input",
-    name: "tests",
-    message: "You can write any available tests for your application here."
+    type: 'input',
+    name: 'tests',
+    message: 'You can write any available tests for your application here.'
 }
-];
+]);
+}
 
-const answ = JSON.stringify(answers)
-// function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile("Readme.md", answ, function(err) {
-        if (err) {
-            throw err;
-        }
-        console.log(answers);
-    })
+function generateMarkdown(answers) {
+    return `
+    # ${answers.title}
+
+    ## Description
+
+    ${answers.description}
+
+    ## Table of Contents
+    * [Installation](##installation)
+    * [Usage](##usage)
+    * [License](##license)
+    * [Contributers](##contributers)
+    * [Tests](##tests)
+
+    ## Installation
+
+    ${answers.install}
+
+    ## Usage
+
+    ${answers.usage}
+
+    ## License
+
+    ${answers.license}
+
+    ## Contributors
+
+    ${answers.contributors}
+
+    ## Tests
+    
+    ${answers.tests}`;
 }
+
+propmtUser()
+    .then(function(answers) {
+        const readMe = generateMarkdown(answers);
+
+        return writeFileAsync("README.md", readMe);
+    })
+    .then(function() {
+        console.log("Successfully wrote a README!");
+    })
+    .catch(function(err) {
+        console.log(err);
+    }
+    )
+// inquire
+// .prompt(questions)
+// .then(answers => {
+//     console.log(JSON.stringify(answers, null, " "));
+// })
+
+// const answ = JSON.stringify(data)
+// function to write README file
+// function writeToFile(fileName, data) {
+//     fs.writeFile("Readme.md", data, function(err) {
+//         if (err) {
+//             throw err;
+//         }
+//         console.log(data);
+//     })
+// }
 
 // function to initialize program
-function init() {
+// function init() {
 
-}
+// }
 
 // function call to initialize program
-init();
+// init();
